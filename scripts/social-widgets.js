ba39bcb9e9c6b0a920d89021b70fcf5cba55b4ea
AnnChou <annreflection@gmail.com>
2025-07-19 15:44:24 -0700

    Create social-widgets.js

---
+++ scripts/social-widgets.js
@@ -0,0 +1,12 @@
+/* ===============================
+☕ Ko-fi Floating Chat Integration
+-------------------------------
+Embeds Ko-fi support button in fixed chat format.
+Widget ID: annreflection
+=============================== */
+kofiWidgetOverlay.draw('annreflection', {
+  type: 'floating-chat',
+  'floating-chat.donateButton.text': 'Support me',
+  'floating-chat.donateButton.background-color': '#f45d22',
+  'floating-chat.donateButton.text-color': '#fff'
+});
